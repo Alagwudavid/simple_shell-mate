52294b900928e109cfa13688e21672d88ea1f24a
Kato Charles <ckcloudia@gmail.com>
2023-07-21 12:45:34 +0300

    refactor: initial the pointer to null so that it isn't pointing to any location

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main(int ac, char **argv)
 {
 	char *prompt = "$ ";
-	char *linepointer = malloc(100); // store buffer address
+	char *linepointer = NULL; // store buffer address
 	size_t n = 0;
 
 	(void)ac; (void)argv;
