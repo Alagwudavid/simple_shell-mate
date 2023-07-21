6302f4b83aee7c92081e056eb06e58d007814504
Kato Charles <ckcloudia@gmail.com>
2023-07-21 12:15:14 +0300

    feat: read user data

--- main.c
+++ main.c
@@ -3,7 +3,12 @@
 int main(int ac, char **argv)
 {
 	char *prompt = "$ ";
+	char *linepointer; // store buffer address
+	size_t n = 0;
 
 	printf("%s", prompt);
+	getline(&linepointer, &n, stdin);
+
+	free(linepointer);
 	return (0);
 }
